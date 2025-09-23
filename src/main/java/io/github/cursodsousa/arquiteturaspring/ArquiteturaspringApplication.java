9e57a02b13a6b14929727f943ecac9448c06983b
Abelardo-Carvalho <abelardocarvalhoneto@gmail.com>
2025-09-23 14:24:31 -0300

    Scope

--- src/main/java/io/github/cursodsousa/arquiteturaspring/ArquiteturaspringApplication.java
+++ src/main/java/io/github/cursodsousa/arquiteturaspring/ArquiteturaspringApplication.java
@@ -2,6 +2,7 @@ package io.github.cursodsousa.arquiteturaspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ArquiteturaspringApplication {
@@ -9,5 +10,6 @@ public class ArquiteturaspringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ArquiteturaspringApplication.class, args);
 	}
+
 
 }
